1a6a760a11311e0f14befa553cfe8572cda44ff3
unknown <gerasimoff.timofeii@yandex.ru>
2022-05-11 05:19:47 +0300

    dev04

---
+++ dev04/task.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	words := []string{"тяпка", "пятак", "пятка", "листок", "слиток", "столик"}
+	fmt.Println(find(words))
+}
+
+func find(words []string) map[string][]string {
+	if len(words) < 2 {
+		return nil
+	}
+	groups := make(map[string][]string)
+	groups[words[0]] = append(groups[words[0]], words[0])
+	for word := 1; word < len(words); word++ {
+		var cur string
+		for k, _ := range groups {
+			var count int
+			iter := make(map[rune]struct{})
+			for _, symb := range []rune(k) {
+				iter[symb] = struct{}{}
+			}
+			for i := 0; i < len([]rune(words[word])); i++ {
+				if _, ok := iter[[]rune(words[word])[i]]; ok {
+					count++
+				} else {
+					break
+				}
+			}
+			if count == len([]rune(k)) {
+				cur = k
+				groups[k] = append(groups[k], words[word])
+				break
+			}
+		}
+		_, ok := groups[cur]
+		if !ok {
+			groups[words[word]] = []string{words[word]}
+		}
+	}
+	return groups
+}
---
+++ dev08/task.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("%v, %v\n", os.Stderr, err)
+		}
+		err = execIn(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func execIn(input string) error {
+	input = strings.TrimSuffix(input, "\r\n")
+
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return fmt.Errorf("No path")
+		}
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	case "pwd":
+		dir, err := os.Getwd()
+		if err != nil {
+			return fmt.Errorf("%v, %v\n", os.Stderr, err)
+		}
+		_, er := fmt.Println(dir)
+		return er
+	case "echo":
+		for i := 1; i < len(args); i++ {
+			_, err := fmt.Printf("%v\n", args[i])
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
