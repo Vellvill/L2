efac018437d5e7cf18ac11b228244f087d58af18
Vellvill <gerasimoff.timofeii@yandex.ru>
2022-06-07 05:46:01 +0300

    patt

---
+++ pat/builder/builder.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type Computer struct {
+	MB  string
+	RAM int
+}
+
+type ComputerBuilderI interface {
+	RAM(int) ComputerBuilderI
+	MB(string) ComputerBuilderI
+
+	Build() Computer
+}
+
+type ComputerBuilder struct {
+	ram int
+	mb  string
+}
+
+func NewComputerBuilder() *ComputerBuilder {
+	return &ComputerBuilder{}
+}
+
+func (c *ComputerBuilder) RAM(ram int) ComputerBuilderI {
+	c.ram = ram
+	return c
+}
+
+func (c *ComputerBuilder) MB(mb string) ComputerBuilderI {
+	c.mb = mb
+	return c
+}
+
+func (c *ComputerBuilder) Build() Computer {
+	return Computer{
+		MB:  c.mb,
+		RAM: c.ram,
+	}
+}
+
+func main() {
+	builder := NewComputerBuilder()
+	comp := builder.RAM(5).MB("gigabyte").Build()
+	fmt.Println(comp)
+}
